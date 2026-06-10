fbf3daebdff4853d527de0110c2adac3b0d56f0f
gleb <tempestusalert@yandex.ru>
2026-06-10 10:30:53 +0300

    Initial Commit

---
+++ haSensorsMenu/haSensorsMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  haSensorsMenuApp.swift
+//  haSensorsMenu
+//
+//  Created by gleb on 10.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct haSensorsMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
